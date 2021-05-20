76ed8317524a56517d1f5e0dfb312237ec1c8c42
JonathanGrossmanITC <61012548+JonathanGrossmanITC@users.noreply.github.com>
2021-05-20 22:32:18 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,198 +1,60 @@
-// This is a console.log, which prints to the browser's console
-console.log("Hi, I'm the console.log");
+console.log("from index.js");
 
-("Hi, I'm the console.log");
+const url = "https://type.fit/api/quotes";
 
-const helloMessage = "Hi, I'm the console.log";
-let goodbyeMessage = "Good bye.";
+const savedQuotes = JSON.parse(sessionStorage.getItem("quotes"));
+// const savedQuotes = JSON.parse(localStorage.getItem("quotes"));
+console.log(savedQuotes);
+if (!savedQuotes) {
+  sessionStorage.setItem("quotes", JSON.stringify([]));
+}
 
-console.log(helloMessage);
-console.log(goodbyeMessage);
+const saveQuote = (event) => {
+  savedQuotes.push(event.target.textContent);
+  sessionStorage.setItem("quotes", JSON.stringify(savedQuotes));
 
-console.log(helloMessage + " " + goodbyeMessage);
-
-goodbyeMessage = "See you.";
-console.log(goodbyeMessage);
-
-const theOtherHelloMessage = "Hi.";
-
-const title = document.getElementById("title");
-console.log(title);
-console.log(title.textContent);
-
-console.log("before", title.classList);
-title.classList.add("text");
-console.log("after", title.classList);
-
-title.classList.remove("title");
-console.log("after remove", title.classList);
-
-// You can change the textContent dynamically
-// title.textContent = "Hello";
-// console.log(title);
-
-// Data types
-
-const aString = "In quotes";
-
-// string vs integer
-const alsoString = "9";
-const integer = 9;
-
-// float vs. integer
-const aFloat = 9.0;
-const anInteger = 9;
-
-// boolean vs. everything
-const aBoolean = true;
-const anotherB = false;
-const notABoolean = "false";
-// const alsoNotABoolean = False;
-
-// array
-const anArray = [
-  "pants",
-  "shirts",
-  "socks",
-  9,
-  10,
-  11,
-  true,
-  ["hi", "shalom", "hola"],
-];
-
-// Using index position with a string
-// const txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// const last = txt.length - 1;
-// console.log(last);
-// console.log(txt[last]);
-
-// let sln = txt.length;
-// console.log("sln 1", sln);
-
-// const empty = "";
-// sln = empty.length;
-// console.log("sln 2", sln);
-
-const str = "Please locate where 'locate' occurs!";
-const pos = str.search("locate");
-console.log(pos);
-
-const text1 = "Hello World!"; // String
-const text2 = text1.toUpperCase(); // "HELLO WORLD!
-console.log(text2);
-
-//Convert string to number data type
-let newNumber = parseInt("999");
-console.log(newNumber);
-
-//Convert string to number data type
-// newNumber = parseFloat("999.0").toFixed(1);
-// console.log(newNumber);
-// title.textContent = newNumber;
-
-// const newBoolean = true;
-// const newString = "true";
-// console.log(typeof newBoolean);
-// console.log(typeof newString);
-
-// if (newBoolean === true) {
-//   console.log("dinner is ready");
-// }
-
-// if (10 > 9) {
-//   console.log(10 > 9);
-// }
-
-// Using index position with a array
-// const thisArray = ["one", "two", "three", "four"];
-// const two = thisArray[1];
-// const lastItem = thisArray[thisArray.length - 1];
-// console.log(two);
-// console.log(lastItem);
-
-// const regularSentence = "Hi there, everybody. I am a sentence.";
-
-// for (i = 0; i < regularSentence.length; i++) {
-//   if (regularSentence[i] === "e") {
-//     console.log(i);
-//   } else {
-//     console.log(regularSentence[i]);
-//   }
-// }
-
-// Live Code Day 6
-const imagesArray = [
-  "terry_crews_one.jpeg",
-  "terry_crews_two.jpeg",
-  "terry_crews_three.jpeg",
-];
-const prevButton = document.getElementById("prevButton");
-const nextButton = document.getElementById("nextButton");
-const image = document.getElementById("largeImage");
-
-let index = 0;
-
-image.src = "./images/" + imagesArray[index]; // ./image/terry_crews_one.jpeg
-console.log("enabled", prevButton.enabled);
-const toggleDisabled = () => {
-  if (index === 0) {
-    prevButton.disabled = true;
-    nextButton.disabled = false;
-
-    prevButton.classList.remove("button-enabled");
-    prevButton.classList.add("button-disabled");
-
-    nextButton.classList.remove("button-disabled");
-    nextButton.classList.add("button-enabled");
-  } else if (index === imagesArray.length - 1) {
-    prevButton.disabled = false;
-    nextButton.disabled = true;
-
-    prevButton.classList.add("button-enabled");
-    prevButton.classList.remove("button-disabled");
-
-    nextButton.classList.add("button-disabled");
-    nextButton.classList.remove("button-enabled");
-  }
+  //   localStorage.setItem("quotes", JSON.stringify(savedQuotes));
 };
 
-const moveBack = () => {
-  console.log("move back");
-  index--;
-  image.src = "./images/" + imagesArray[index];
-  // Check whether index is equal to the index position of the first item in our array
-  // Implemented this before using the toggleDisable()
-  // if (nextButton.disabled !== false) {
-  //   nextButton.disabled = false;
-  // }
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const quotesWrapper = document.getElementById("quotesWrapper");
+    for (let i = 0; i <= 9; i++) {
+      const quoteItem = document.createElement("div");
+      quoteItem.classList.add("quote-item");
 
-  toggleDisabled();
-  if (index === 0) {
-    // Disable the previous button because no more items after it
-    prevButton.disabled = true;
-  }
-  return "hi guys";
-};
+      const quote = document.createElement("div");
+      quote.textContent = data[i]["text"];
+      quote.classList.add("quote");
+      quote.addEventListener("click", saveQuote);
 
-const moveForward = () => {
-  // console.log("move forward");
-  index++;
-  // console.log(index);
-  image.src = "./images/" + imagesArray[index];
-  // The array.length - 1 is always the last item in the array
-  // Check whether index is equal to the index position of the last item in our array
+      const author = document.createElement("div");
+      author.textContent = data[i]["author"];
+      author.classList.add("author");
 
-  // Implemented this before using the toggleDisable()
-  // if (prevButton.disabled !== false) {
-  //   prevButton.disabled = false;
-  // }
-  toggleDisabled();
-  if (index === imagesArray.length - 1) {
-    // Disable the next button because no more items after it
-    nextButton.disabled = true;
-  }
-};
+      quoteItem.append(quote, author);
+      quotesWrapper.append(quoteItem);
+    }
+  });
 
-prevButton.addEventListener("click", moveBack);
-nextButton.addEventListener("click", moveForward);
+const pathnameURL = window.location.pathname;
+
+// One way to do this:
+const pathnameURLArray = pathnameURL.split("/");
+const lastIndex = pathnameURLArray.length - 1;
+const lastItem = pathnameURLArray[lastIndex];
+const titleFromUrl = lastItem.replace(".html", "").replace("_", " ");
+
+// Another way to do this:
+// const titleFromUrl = pathnameURL
+//   .replace("/Users/jonathangrossman/Desktop/first_steps_april_live/", "")
+//   .replace(".html", "")
+//   .replace("html/", "")
+//   .replace("_", " ");
+
+console.log(titleFromUrl);
+
+const titleHtml = document.getElementById("title");
+titleHtml.textContent = titleFromUrl;
+console.log(titleFromUrl);
