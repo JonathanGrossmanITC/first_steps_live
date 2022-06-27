2259ecb70d90287c8cf22f6f49d7dd3ca19e1a78
JonathanGrossmanITC <61012548+JonathanGrossmanITC@users.noreply.github.com>
2022-06-27 21:21:26 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Once something is saved in memory, you can interact with it in your JavaScript code
+// You can save any data type to a variable
+// Once something is saved as a variable, the variable represents that thing in memory, which means the variable can do the things that the underlying data is capable of
+// -- For instance, a number stored in a variable: you can add, subtract, multiply, divide and more using the variable
+// -- Another is example is a string stored in a variable: you can use that variable to set the text of an HTML element
